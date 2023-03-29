3a105a1e78a5585c25c0afc37a5bd7253ce75143
Šimon Ugor <simonugor@MacBook-Air-uzivatela-Simon.local>
2023-03-29 21:35:03 +0200

    pages head update

--- src/pages/oaplikacii.tsx
+++ src/pages/oaplikacii.tsx
@@ -1,23 +1,32 @@
 import Navbar from '@/components/navbar'
 import React from 'react'
+import Head from 'next/head'
 
 const oaplikacii = () => {
   return (
-    <div className='h-screen w-screen grid grid-rows-2'>
-        <Navbar searchFilterHidden={true} />
-        <div className='bg-neutral-content flex items-center flex-col'>
-            <h1 className='mt-2 text-3xl underline'>Tech stack</h1>
-            <div className='mt-1 text-center text-xl'>
-                <p>NextJS</p>
-                <p>Tailwind CSS</p>
-                <p>DaisyUI</p>
-                <p>Zod</p>
-                <p>FetchAPI</p>
-                <p>Mockapi.io</p>
-                <p>Vercel</p>
-            </div>
-        </div>
-    </div>
+    <>
+      <Head>
+        <title>ToDo App - Tech stack</title>
+        <meta name="description" content="ToDo Application - tech stack" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='h-screen w-screen grid grid-rows-2'>
+          <Navbar searchFilterHidden={true} />
+          <div className='bg-neutral-content flex items-center flex-col'>
+              <h1 className='mt-2 text-3xl underline'>Tech stack</h1>
+              <div className='mt-1 text-center text-xl'>
+                  <p>NextJS</p>
+                  <p>Tailwind CSS</p>
+                  <p>DaisyUI</p>
+                  <p>Zod</p>
+                  <p>FetchAPI</p>
+                  <p>Mockapi.io</p>
+                  <p>Vercel</p>
+              </div>
+          </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/zdrojovykod.tsx
+++ src/pages/zdrojovykod.tsx
@@ -1,5 +1,6 @@
 import Navbar from '@/components/navbar'
 import React, { useEffect } from 'react'
+import Head from 'next/head'
 
 const zdrojovykod = () => {
 
@@ -8,10 +9,18 @@ const zdrojovykod = () => {
     })
 
   return (
-    <div className='h-screen w-screen grid grid-rows-2'>
-        <Navbar searchFilterHidden={true} />
-        <div className='bg-neutral-content h-full'></div>
-    </div>
+    <>
+      <Head>
+        <title>ToDo App - Zdrojový kód</title>
+        <meta name="description" content="ToDo Application - zdrojový kód" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='h-screen w-screen grid grid-rows-2'>
+          <Navbar searchFilterHidden={true} />
+          <div className='bg-neutral-content h-full'></div>
+      </div>
+    </>
   )
 }
 
